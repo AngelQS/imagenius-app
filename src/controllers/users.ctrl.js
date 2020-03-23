f53a70dfb8c2c4f89e6d1140455b24b768bc7675
AngelQS <aedwin.acuario31@gmail.com>
2020-03-23 17:24:52 -0500

    MongoDB User model added

--- src/controllers/users.ctrl.js
+++ src/controllers/users.ctrl.js
@@ -10,14 +10,15 @@ usersCtrl.renderSignUpForm = (req, res) => {
 
 usersCtrl.signUp = (req, res, next) => {
   try {
+    // Data input verification
     const result = userValidationSchema.validate(req.body);
+
+    // Data input validation
     if (result.error) {
       req.flash('error', result.error.details[0].message);
       console.log('req.body:', req.body);
       console.log('validation error:', result.error.details[0].message);
       res.send('Error on validation!');
-    } else {
-      res.send('Successfull validation!');
     }
   } catch (err) {
     next(err);
---
+++ src/models/users.model.js
@@ -0,0 +1,55 @@
+// Third
+const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new Schema(
+  {
+    fullname: {
+      type: String,
+      required: true,
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    secretToken: {
+      type: String,
+      default: null,
+    },
+    active: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true },
+);
+
+// Schema methods
+userSchema.methods.encryptPassword = async (password) => {
+  try {
+    const salt = await bcrypt.genSalt(12);
+    return await bcrypt.hash(password, salt);
+  } catch (err) {
+    throw
+  }
+};
+
+userSchema.methods.matchPassword = async function(password){
+  try {
+    return await bcrypt.compare(password, this.password);
+  } catch (err) {
+    throw new Error('Passwords are not equals.', err)
+  }
+}
+
+module.exports = model('user', userSchema);
